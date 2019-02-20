43b47629ce6c776dfd0a0c75e672efdc7c05d600
Sindre Sorhus <sindresorhus@gmail.com>
2019-02-20 13:10:57 +0700

    Prevent the computer from sleeping while recording
    See https://github.com/wulkano/Aperture/commit/67d0743770e59e094779a5076a64b770bd94e36b

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/wulkano/Aperture", from: "0.1.0")
+    .package(url: "https://github.com/wulkano/Aperture", from: "0.2.0")
   ],
   targets: [
     .target(
